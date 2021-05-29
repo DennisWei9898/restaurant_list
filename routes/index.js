9dfb44ae65e83c4970b78a03c88c2d3cdefefae1
Dennis Wei <denniseasycard@gmail.com>
2021-05-29 18:22:53 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,12 @@ const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const sort = require('./modules/sort')
 const search = require('./modules/search')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/sort', sort)
 router.use('/search', search)
+router.use('/users', users)
 
 module.exports = router
